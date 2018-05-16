19517f3aab6e44998f778a17231e7c829dccaa13
Cj-bc <cj-bc@y-modify.org>
2018-05-16 14:09:43 +0900

    Add: libstandard> functions> error, is_error, init

---
+++ libstandard.sh
@@ -0,0 +1,53 @@
+# libstandard.sh  -- group of standard functions
+#
+# ver: 0.0.1
+# tags: standard
+# desc: help to make your library.
+#
+# copyright (c) 2018 Cj-bc
+# This software is released under MIT License.
+
+
+# output error message to stderr
+# @stderr string error message
+# @param <string error_message>
+# @return 0 success
+function error {
+  echo "$@" >2
+  return 0
+}
+
+# check if previous command fail and execute command
+# @param <function error_op> [<function success_op>]
+# @return 0 success
+# @return 1 function not found
+function is_error {
+  local status=$?
+  local error_operation=()
+  for arg in "$@";do
+    [ "$arg" = "--" ] && shift && break
+    error_operation=(${error_operation[@]} $arg)
+    shift
+  done
+  local success_operation=($@)
+
+  if [ $status -eq 0 ]; then
+    ${success_operation[@]} || return 1
+  else
+    ${error_operation[@]} || return 1
+  fi
+  return 0
+}
+
+
+# execute all <lib>.init at first. Won't be called by user.
+# @param <string lib1> <string lib2> ...
+# @return 0 success
+# @return 1 fail in some library
+# @return 2 fail in this function
+function init {
+  for lib in "$@";do
+    ${lib}.init || ~error "fail: sourcing ${lib}" && return 1
+  done
+  return 0
+}
