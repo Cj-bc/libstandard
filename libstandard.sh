4c728fce6865bf35eb20a84f6b67c14bc97d7814
Cj-bc <cj-bc@y-modify.org>
2018-07-18 18:30:24 +0900

    Add: error codes based on /usr/include/sysexits.h

--- libstandard.sh
+++ libstandard.sh
@@ -8,6 +8,23 @@
 # This software is released under MIT License.
 
 
+
+EX_USAGE=64
+EX_DATAERR=65
+EX_NOINPUT=66
+EX_NOUSER=67
+EX_NOHOST=68
+EX_UNAVAILABLE=69
+EX_SOFTWARE=70
+EX_OSERR=71
+EX_OSFILE=72
+EX_CANTCREAT=73
+EX_IOERR=74
+EX_TEMPFAIL=75
+EX_PROTOCOL=76
+EX_NOPERM=77
+EX_CONFIG=78
+
 # output error message to stderr
 # @stderr string error message
 # @param <int error_code> <string error_message>
